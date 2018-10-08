501cbd6aba3403c56871590d8631a6a8d3202848
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-08 14:50:27 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,7 +1,7 @@
-def introduction(name) 
+def introduction(Josh) 
   puts "Hi, my name is #{name}"
 end
 
-def introduction_with_language(name, language) 
+def introduction_with_language(Dan, Ember.js) 
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
